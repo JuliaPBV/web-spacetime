2a54b16832ba37ad2b5b8fd02e4ce97f06577726
Julia Pereira <vital.pereira.julia@gmail.com>
2025-03-24 13:09:14 -0300

    componentes e propriedades

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import { Button } from "@/components/Button";
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <h1>
+      Hello World
+      <Button title="Julia"/>
+      <Button title="Anna"/>
+      <Button title="clara"/>
+
+      </h1>
   )
 }
