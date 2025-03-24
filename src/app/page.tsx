5d91ba1f697b2793b749ce6f1c48103585e83105
Julia Pereira <vital.pereira.julia@gmail.com>
2025-03-24 14:37:37 -0300

    prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { Button } from "@/components/Button";
-
 export default function Home() {
   return (
-    <h1>
-      Hello World
-      <Button title="Julia"/>
-      <Button title="Anna"/>
-      <Button title="clara"/>
-
-      </h1>
+    <div className="h-screen bg-zinc-950 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+    </div>
   )
 }
