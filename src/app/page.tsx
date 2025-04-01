6095f3c13ce9090bd500931c73e7043f221d84aa
Julia Pereira <vital.pereira.julia@gmail.com>
2025-04-01 13:03:38 -0300

    obtendo o código do github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,8 @@ export default function Home() {
           <div className="absolute right-2 top-0 bottom-0 w-2 bg-[linear-gradient(to_bottom,rgba(255,255,255,0.1),rgba(255,255,255,0.1)_12.5%,transparent_12.5%,transparent)] bg-[length:100%_8px]" />
           
           {/* Sign In */}
-          <a href="" className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
+          <a href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
+          className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
             <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
               <User className='h-5 w-5 text-gray-500'/>
             </div>
